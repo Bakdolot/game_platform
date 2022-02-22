5fca77a9849200f878669ff321c88d5bbbc75a8a
Bakdolot <genry03gh@gmail.com>
2022-02-22 14:56:49 +0600

    fixed static bug

--- gaming_platform/settings.py
+++ gaming_platform/settings.py
@@ -162,10 +162,10 @@ STATIC_URL = '/static/'
 
 # Add these new lines
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Base url to serve media files
 MEDIA_URL = '/media/'
